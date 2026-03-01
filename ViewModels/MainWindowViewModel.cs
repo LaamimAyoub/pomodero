7644262692bf6686a4f2898e84a2d4490daa5cc8
Ayb <ayoublaamim1996@gmail.com>
2026-03-01 00:10:53 +0000

    init commit

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace pomodero.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace pomodero.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
